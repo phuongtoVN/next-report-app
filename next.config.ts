0055cba47b91091507e27da1c222ce3bf17fe9aa
phuongtoVN <toyenphuong10x@gmail.com>
2025-01-09 12:53:05 -0800

    tesing next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,8 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  // output: "export", // This ensures `next export` generates static files
-  // assetPrefix: isProd ? "/next-report-app/" : "", 
-  // images: {
-  //   unoptimized: true, // Disable image optimization for static export
-  // },
+  output: 'export',
+  basePath: '/next-report-app',
 };
 
 export default nextConfig;
