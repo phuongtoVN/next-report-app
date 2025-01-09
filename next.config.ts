ea9c8bd42972e3e6b41f8a9a9f1f0d832118e336
phuongtoVN <toyenphuong10x@gmail.com>
2025-01-09 12:35:30 -0800

    debug testing no nextconfig

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  output: "export", // This ensures `next export` generates static files
-  assetPrefix: isProd ? "/next-report-app/" : "", 
-  images: {
-    unoptimized: true, // Disable image optimization for static export
-  },
+  // output: "export", // This ensures `next export` generates static files
+  // assetPrefix: isProd ? "/next-report-app/" : "", 
+  // images: {
+  //   unoptimized: true, // Disable image optimization for static export
+  // },
 };
 
 export default nextConfig;
