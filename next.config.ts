de2fe2b06b16dc97a681e0f0e3aeffdcb28cda8b
phuongtoVN <toyenphuong10x@gmail.com>
2025-01-09 18:43:41 -0800

    add condition so next start can run with output: export

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,13 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/next-report-app',
-};
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/next-report-app/' : '',
+  basePath: isProd ? '/next-report-app' : '',
+  output: process.env.NEXT === "true" ? "export" : undefined,
+  };
 
 export default nextConfig;
